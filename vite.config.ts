fca56fb73039fed04fa87011f991f844c13a17aa
Jeremias Fernandez <jereferdz@gmail.com>
2025-12-18 21:37:31 -0300

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Arena-De-Leyendas/',
+      base: '/testing-ia-game/',
       server: {
         port: 3000,
         host: '0.0.0.0',
